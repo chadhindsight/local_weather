8330513c3e161bf3f52986eea564eac7d57ce2bd
Chad Hinds <chadrickj8@gmail.com>
2019-07-04 16:19:19 -0400

    add views folder

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000);
+// dictates what happens when "/" route is requested
+app.get("/",function (req, res) {
+    res.send("<p>hello world</p>");
+})
+
+app.get("/about", function (req, res) {
+  res.send("Hi I love booty")  
+})
+// setup server on a certain port
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});

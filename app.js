295806eb6d41953d1c1f65f46cb78dc49e6c748e
Chad Hinds <chadrickj8@gmail.com>
2019-07-04 22:45:14 -0400

    fix path in res.sendFile

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const app = express();
 
 // dictates what happens when "/" route is requested
 app.get("/",function (req, res) {
-    res.send("<p>hello world</p>");
+    res.sendFile(__dirname + "/index.html")
 })
 
-app.get("/about", function (req, res) {
-  res.send("Hi I love booty")  
-})
 // setup server on a certain port
 app.listen(3000, function() {
     console.log("Server started on port 3000");

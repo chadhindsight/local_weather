ee0d6f7361af147222f8e2578bcb9fe94dd110d4
Chad Hinds <chadrickj8@gmail.com>
2019-07-05 14:41:52 -0400

    get app.post ready

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.get("/",function (req, res) {
     res.sendFile(__dirname + "/index.html")
 })
 
+app.post()
+
 // setup server on a certain port
 app.listen(3000, function() {
     console.log("Server started on port 3000");

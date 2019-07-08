d3a5d7d32caffc0448583919bbd948429f720edd
Chad Hinds <chadrickj8@gmail.com>
2019-07-08 11:00:04 -0400

    use geo-lite

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
+
 // parse the json that you get
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))

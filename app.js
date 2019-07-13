46395f89ece965bed6497b69c9e46a4b05de8d2d
Chad Hinds <chadrickj8@gmail.com>
2019-07-13 11:16:47 -0400

    fix comment typo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// parse the json that you get
+// parse the json that you get!
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 

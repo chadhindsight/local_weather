30e6231b57809e2c4f6a4eff59e6494287b6dc51
Chad Hinds <chadrickj8@gmail.com>
2019-08-19 11:34:04 -0400

    change comment about req.ip

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const API_KEY = '18436a5aee03555399b6774854293b06';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 app.post("/", function (req, res) {
-    // Under normal circumstances you would use req.ip to get ip address, but here it is localhost
+    // Under normal circumstances you would use req.ip to get ip address, but here we dont use it because localhost
     let url = `${ROOT_URL}&q=bronx,us`
 
     async function getWeather() {
@@ -36,7 +36,7 @@ app.post("/", function (req, res) {
                 let celsius = Math.round(temperature - 273.15);
                 let fahrenheit = Math.round((temperature - 273.15) * 1.8 + 32);
                 // Render view and sends the relevant info and template to the client
-                res.render("forecast", { weather, temperature });
+                res.render("forecast", { weather, celsius, fahrenheit});
             }
 
         });        

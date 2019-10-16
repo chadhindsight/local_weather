145072ba5bb926ca648c8b0a919ad61efb256a05
Chad Hinds <chadrickj8@gmail.com>
2019-10-16 09:33:42 -0400

    Setup test for post request

--- test/test.js
+++ test/test.js
@@ -4,9 +4,13 @@ const request = require('supertest');
 // Include your express app 
 const app = require('../app');
 
-// Test for the rendering of home page route
-describe('GET the main page', function (){
+// Test for the rendering of home page route!
+describe('GET the main page & show weather', function (){
     it('renders the main page', (done) =>{
         request(app).get('/').expect(200, done);
+    });
+
+    it('upon clicking button post request is successfully made', (done) => {
+        request(app).post('/').expect(200, done);
     });
 });

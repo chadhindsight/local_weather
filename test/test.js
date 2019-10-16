07f5dfc9510463e09de2d6d45311a543f62815d4
Chad Hinds <chadrickj8@gmail.com>
2019-10-16 09:26:34 -0400

    Add logic thats tests for the rendering of the home page

--- test/test.js
+++ test/test.js
@@ -1,2 +1,12 @@
 /*jshint esversion: 8 */
 const expect = require('chai').expect;
+const request = require('supertest');
+// Include your express app 
+const app = require('../app');
+
+// Test for home page route
+describe('GET the main page', function (){
+    it('renders the main page', (done) =>{
+        request(app).get('/').expect(200, done);
+    });
+});

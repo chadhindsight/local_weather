0884db9c8e23d3cbabc0ae8d08741338b572b54f
Chad Hinds <chadrickj8@gmail.com>
2019-12-29 09:04:38 -0500

    Rename the api response variable to information because that is more defined

--- app.js
+++ app.js
@@ -27,15 +27,15 @@ app.post("/", function (req, res) {
 
     async function getWeather() {
        try {
-           let re = await axios.get(url);
+           let information = await axios.get(url);
 
-                   let todayWeather = JSON.stringify(re.data.list[0].weather);
+                   let todayWeather = JSON.stringify(information.data.list[0].weather);
                    let forecast = JSON.parse(todayWeather);
 
                    for (var i = 0; i < forecast.length; i++) {
                        const weather = forecast[i]['main'];
                        const displayIcon = `http://openweathermap.org/img/wn/${forecast[i]['icon']}@2x.png`;
-                       const temperature = re.data.list[0].main.temp;
+                       const temperature = information.data.list[0].main.temp;
                     
                        let celsius = Math.round(temperature - 273.15);
                        let fahrenheit = Math.round((temperature - 273.15) * 1.8 + 32);

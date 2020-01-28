69fc94c323a4b29ffc9110c367952673ed61af28
Chad Hinds <chadrickj8@gmail.com>
2020-01-28 15:02:38 -0500

    comment on what the get method does  when the / route is accessed

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Dictates what the response is when "/" route gets requested
+// Dictates what the response is when the "/" route gets requested
 app.get("/", function (req, res) {
     res.render('main');
 });

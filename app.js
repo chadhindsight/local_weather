2993fc2697b4399802bc15aace98c8afc92a3a67
Chad Hinds <chadrickj8@gmail.com>
2020-02-23 09:38:20 -0500

    Change the name of the variable that stores the png weather icon

--- app.js
+++ app.js
@@ -34,14 +34,14 @@ app.post("/", function (req, res) {
 
                    for (var i = 0; i < forecast.length; i++) {
                        const weather = forecast[i].main;
-                       const displayIcon = `http://openweathermap.org/img/wn/${forecast[i]['icon']}@2x.png`;
+                       const iconDisplay = `http://openweathermap.org/img/wn/${forecast[i]['icon']}@2x.png`;
 
                        const temperature = information.data.list[0].main.temp;
 
                        let celsius = Math.round(temperature - 273.15);
                        let fahrenheit = Math.round((temperature - 273.15) * 1.8 + 32);
 
-                       res.render("forecast", { weather, celsius, fahrenheit, displayIcon });
+                       res.render("forecast", { weather, celsius, fahrenheit, iconDisplay });
                    }
 
                
